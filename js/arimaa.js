5efe872dfeaa0e75157d57ddad1c5a0a8c146c4d
egaga <henrik.huttunen@gmail.com>
2010-09-04 12:35:11 +0300

    initial code

---
+++ js/arimaa.js
@@ -0,0 +1,12 @@
+var ARIMAA = ARIMAA || function() {
+
+	var empty = { 'side': 'none'; } /* FIXME: this is not good way to encode empty square */
+  var	silver = { 'side': 'silver' }
+  var gold = { 'side': 'gold' }
+  
+  return {
+  	'silver': silver,
+  	'gold': gold
+  }
+  
+}();
