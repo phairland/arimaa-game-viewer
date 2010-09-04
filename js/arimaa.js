d26779cb9161f67845731d09dbe1fea051f9a7df
egaga <henrik.huttunen@gmail.com>
2010-09-04 18:13:16 +0300

    some enchancements

--- js/arimaa.js
+++ js/arimaa.js
@@ -1,5 +1,7 @@
 var ARIMAA = ARIMAA || function() {
 
+	var board_width = 8;
+	var board_height = 8;
   var	silver = { 'side': 'silver' }
   var gold = { 'side': 'gold' }
   
@@ -16,7 +18,51 @@ var ARIMAA = ARIMAA || function() {
   	}
   }
   
+  /** Copies the board structure (immutability guarantees less problems) */
+  function copy_board(old) {
+  	return GENERIC.map(old, function(row) {
+  		return GENERIC.map(row, function(elem) { return elem; });
+  	});
+  }
+  
+  function move_piece(board, piece_coordinate, new_coordinate) {
+  	 var new_board = copy_board(board);
+  	 var piece = new_board[piece_coordinate.col][piece_coordinate.row];
+  	 new_board[piece_coordinate.col][piece_coordinate.row] = {}; // takes piece away from old place
+  	 new_board[new_coordinate.col][new_coordinate.row] = piece;
+  	 return new_board;
+  }
+  
+  function is_empty_square(square) { return square.type === undefined; }
+  function legal_moves(board, coordinate) {
+		function get_x(direction) { return direction[0]; }
+		function get_y(direction) { return direction[1]; }
+
+  	var left = [-1, 0];
+  	var right = [1, 0];
+  	var north = [0, -1];
+  	var south = [0, 1];
+  	
+  	function can_move_to(direction) {
+  		var x = coordinate.row + get_x(direction);
+  		var y = coordinate.col + get_y(direction);
+  		
+  		if(x < 0 || y < 0 || x >= board_width || y >= board_height) return false;
+  		return is_empty_square(board[x][y]); //FIXME: also pushing and pulling should be considered
+  	}
+  	
+  	var legal_directions = GENERIC.filter([left, right, north, south], can_move_to);
+  	return GENERIC.map(legal_directions, function(direction) { 
+  			return {
+  				'row': coordinate.row + get_x(direction),
+  				'col': coordinate.col + get_y(direction)
+  			}
+  	});
+  }
+  
   return {
+  	'board_width': board_width,
+  	'board_height': board_height,
   	'silver': silver,
   	'gold': gold,
   	'rabbit': rabbit,
@@ -24,7 +70,9 @@ var ARIMAA = ARIMAA || function() {
   	'dog': dog,
   	'horse': horse,
   	'camel': camel,
-  	'elephant': elephant
+  	'elephant': elephant,
+  	'legal_moves': legal_moves,
+  	'move_piece': move_piece
   }
   
 }();
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 var ARIMAA_MAIN = ARIMAA_MAIN || function() {
 	var board = init_arimaa_board()['board'];
+	var selected = undefined; // what square is selected currently
 
 	//FIXME: piece testing should be in arimaa logic
 	function is_piece(piece) { return piece !== undefined && piece['type'] !== undefined; }
@@ -41,7 +42,67 @@ var ARIMAA_MAIN = ARIMAA_MAIN || function() {
 			$(this).toggleClass('selected');
 		});
 	}
+
+	function bind_select_piece() {
+		$('.square').live('mouseover', function() {
+			selected = coordinate_for_element($(this));
+			show_arrows($(this));
+		});
+	}
+
+	function coordinate_for_element(elem) {
+		return {
+			'row': row_index(elem),
+			'col': col_index(elem)
+		}		
+	}
 	
+	function coordinates_from_arrow(arrow) {
+		return {
+			'row': arrow.attr('row'),
+			'col': arrow.attr('col')
+		}
+	}
+	
+	function bind_move_piece() {
+		$('.arrow').click(function() {
+			var new_coordinate = coordinates_from_arrow($(this));
+			console.log(new_coordinate);
+			move_piece(selected, new_coordinate);
+		});
+	}
+	
+	function move_piece(selected, new_coordinate) {
+		if(selected === undefined) return;
+		board = ARIMAA.move_piece(board, selected, new_coordinate);
+	}
+	
+	function show_arrows(elem) {
+		var coordinate = {
+		 'row': row_index(elem),
+		 'col': col_index(elem)
+		}
+		
+		var possible_moves = ARIMAA.legal_moves(board, coordinate);
+
+		console.log(possible_moves);
+		
+		GENERIC.for_each(possible_moves, function(move) {
+		});
+
+		var center_x = elem.position().left + elem.width() / 2 - $('.arrow').width() / 2;
+    var center_y = elem.position().top + elem.height() / 2 - $('.arrow').height()/2;
+
+    $('.arrow')
+      .attr('row', coordinate.row)
+      .attr('col', coordinate.col);
+      
+    $('.arrow')
+       .css('left', center_x)
+       .css('top', center_y)
+       .show();		
+	}
+		
 	function col_index(elem) {
 		return $('.row').index(elem.closest('.row'));
 	}
@@ -55,6 +116,7 @@ var ARIMAA_MAIN = ARIMAA_MAIN || function() {
 	$(function() {
 		show_board(board);
 		bind_select_piece();
+		bind_move_piece();
 	});
 	
 }();
